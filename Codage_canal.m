c27952a8db4fce6506ec7bd57fd86b78bd3302d8
yra7512 <yan.rabefaniraka@etu.inp-n7.fr>
2025-05-07 10:51:09 +0200

    Initial commit

---
+++ Codage_canal.m
@@ -0,0 +1,203 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ETUDE D'UN CODAGE DE TYPE BLOC ET D'UN CODAGE CONVOLUTIF
+% COMPARAISON CHAINE CODEE / CHAINE NON CODEE
+% RABEFANIRAKA Yan, Mai 2025
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear
+close all
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%PARAMETRES GENERAUX 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Fe = 12000;       %Fréquence d'échantillonnage
+Te = 1/Fe;        %Période d'échantillonnage
+Rb = 3000;        %Débit binaire souhaité
+N = 1000;         %Nombre de bits générés
+
+M = 2;         %Ordre de la modulation          
+Rs = Rb / log2(M);         %Débit symbole   
+Ns = 1 / (Rs * Te);         %Facteur de suréchantillonnage
+
+%tableau des valeurs de SNR par bit souhaité à l'entrée du récpeteur en dB
+tab_Eb_N0_dB = [2: 8]; 
+%Passage au SNR en linéaire
+tab_Eb_N0 = 10 .^ (tab_Eb_N0_dB / 10); 
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BOUCLE SUR LES NIVEAUX DE Eb/N0 A TESTER
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for indice_bruit = 1:length(tab_Eb_N0_dB)
+
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % VALEUR DE Eb/N0 TESTEE
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Eb_N0_dB = tab_Eb_N0_dB(indice_bruit)
+    Eb_N0 = tab_Eb_N0(indice_bruit);
+
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % INITIALISATIONS
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    nb_erreurs = 0;   %Variable permettant de compter le nombre d'erreurs cumulées
+    nb_cumul = 0;     %Variables permettant de compter le nombre de cumuls réalisés
+    TES_BPSK = 0;
+    TEB_BPSK = 0;
+    
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % BOUCLE POUR PRECISION TES ET TEBS MESURES :COMPTAGE NOMBRE ERREURS
+    % (voir annexe texte TP)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % while(nb_erreurs < 1000)
+
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %GENERATION DE L'INFORMATION BINAIRE
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        bits = randi([0, 1], 1, N);
+        
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %MAPPING 
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Mapping BPSK
+        symboles_BPSK = pskmod(bits, M, 0, InputType="bit", PlotConstellation=true);
+        a_k = real(symboles_BPSK);
+        b_k = zeros(size(a_k));
+
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %SURECHANTILLONNAGE
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        somme_Diracs_ponderes_BPSK = kron(a_k, [1 zeros(1, Ns-1)]);
+        
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %FILTRAGE DE MISE EN FORME 
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Génération de la réponse impulsionnelle du filtre de mise en forme
+        h= ones(1, Ns);
+
+        %Filtrage de mise en forme
+        Signal_emis_BPSK=filter(h, 1, somme_Diracs_ponderes_BPSK);
+        
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %CANAL DE PROPAGATION AWGN
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %POUR MODULATION BPSK
+        %Calcul de la puissance du signal émis en BPSK
+        P_signal = (1/ (2 * N + 1)) * sum(norm(Signal_emis_BPSK).^2);
+
+        %Calcul de la puissance du bruit à ajouter au signal pour obtenir la valeur
+        %souhaité pour le SNR par bit à l'entrée du récepteur (Eb/N0)
+        P_bruit = (P_signal * Ns) / (2 * log2(M) * Eb_N0);
+
+        %Génération du bruit gaussien à la bonne puissance en utilisant la fonction
+        %randn de Matlab (bruit réel ici car en BPSK l'enveloppe complexe est réelle)
+        Bruit =sqrt(P_bruit) * randn(1, length(Signal_emis_BPSK));
+
+        %Ajout du bruit canal au signal émis => signal à l'entrée du récepteur
+        Signal_recu_BPSK = Signal_emis_BPSK;
+        % Signal_recu_BPSK = Signal_emis_BPSK + Bruit;
+        
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %FILTRAGE DE RECEPTION ADAPTE
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Réponse impulsionnelle du filtre de réception
+        hr = ones(1, Ns);
+
+        %Filtrage de réception
+        Signal_recu_filtre_BPSK = filter(hr, 1, Signal_recu_BPSK);
+        figure
+        plot(Signal_recu_filtre_BPSK)
+        
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %ECHANTILLONNAGE
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Choix de n0 
+        n0 = 4;     % A COMPLETER   
+        %Echantillonnage à n0+mNs
+        Signal_echantillonne_BPSK = Signal_recu_filtre_BPSK(n0:Ns:end);
+        
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %DECISIONS SUR LES SYMBOLES 
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        symboles_recus_decimaux_BPSK = real(Signal_echantillonne_BPSK) > 0;
+        
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %CALCUL DU TAUX D'ERREUR SYMBOLE CUMULE
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        TES_BPSK = TES_BPSK + sum( symboles_BPSK ~= symboles_recus_decimaux_BPSK) / length(symboles_BPSK);
+
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %DEMAPPING
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        bits_recus_BPSK = pskdemod(Signal_echantillonne_BPSK, M);
+
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %CALCUL DU TAUX D'ERREUR BINAIRE CUMULE
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        TEB_BPSK = TEB_BPSK + sum( bits ~= bits_recus_BPSK) / length(bits);
+        
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %CUMUL DU NOMBRE D'ERREURS ET NOMBRE DE CUMUL REALISES
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        nb_erreurs=nb_erreurs + sum(bits ~= bits_recus_BPSK);
+        nb_cumul = nb_cumul + 1;
+
+    % end  %fin boucle sur comptage nombre d'erreurs
+
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %CALCUL DU TAUX D'ERREUR SYMBOLE ET DU TAUX D'ERREUR BINAIRE POUR LA
+    %VALEUR TESTEE DE Eb/N0
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    TES_simule_BPSK(indice_bruit) = TES_BPSK / nb_cumul;
+    TEB_simule_BPSK(indice_bruit) = TEB_BPSK / nb_cumul 
+
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %DIAGRAMME DE L'OEIL EN SORTIE DU FILTRE DE RECEPTION AVEC BRUIT
+    %TRACE POUR CHAQUE VALEUR DE Eb/N0
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %MODULATION 4-ASK
+    oeil=reshape(Signal_recu_filtre_BPSK, Ns, length(Signal_recu_filtre_BPSK) / Ns);
+    figure
+    plot(oeil)
+    title(['Tracé du diagramme de l"oeil en sortie du filtre de réception (BPSK) pour E_b/N_0 = ' num2str(Eb_N0_dB) 'dB'])
+
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %TRACE DES CONSTELLATIONS APRES ECHANTILLONNAGE POUR CHAQUE VALEUR DE Eb/N0
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %MODULATION BPSK
+    figure
+    plot(a_k, b_k, 'b*');
+    xlabel('a_k')
+    ylabel('b_k')
+    title(['Tracé de la constellation en sortie du filtre de réception (BPSK) pour E_b/N_0 = ' num2str(Eb_N0_dB) 'dB'])
+
+end  %fin boucle sur les valeurs testées de Eb/N0
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%TES ET TEB THEORIQUES CHAINES IMPLANTEES
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+TES_THEO_BPSK = qfunc(sqrt(2 * Eb_N0));
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%CALCUL DU TES ET TEB THEORIQUE DE LA CHAINE IMPLANTEE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+TEB_THEO_BPSK = TES_THEO_BPSK
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%TRACES DES TES ET TEB OBTENUS EN FONCTION DE Eb/N0
+%COMPARAISON AVEC LES TES et TEBs THEORIQUES
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure
+semilogy(tab_Eb_N0_dB, TES_THEO_BPSK,'r-x')
+hold on
+semilogy(tab_Eb_N0_dB, TES_simule_BPSK,'b-o')
+legend('TES théorique BPSK','TES simulé BPSK')
+xlabel('E_b/N_0 (dB)')
+ylabel('TES')
+
+figure
+semilogy(tab_Eb_N0_dB, TEB_THEO_BPSK,'r-x')
+hold on
+semilogy(tab_Eb_N0_dB, TEB_simule_BPSK,'b-o')
+legend('TEB théorique BPSK','TEB simulé BPSK')
+xlabel('E_b/N_0 (dB)')
+ylabel('TEB')
